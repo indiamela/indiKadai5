4a5226102c9fb6e2cf45c43597b0c89a295d1397
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-12-05 09:31:53 +0900

    add-calcAction

--- indiKadai5/ViewController.swift
+++ indiKadai5/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var dividendTextField: UITextField!
+    @IBOutlet private weak var divisorTextField: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+
+    @IBAction func onTapCalcButton(_ sender: Any) {
+        do {
+            try calcAction()
+        } catch CalcError.dividendTextIsEmpty {
+            showErrorAlert("割られる数を入力してください")
+        } catch CalcError.divisorTextIsEmpty {
+            showErrorAlert("割る数を入力してください")
+        } catch CalcError.divisorIsZero {
+            showErrorAlert("割る数には0を入力しないでください")
+        } catch {
+            showErrorAlert("予期しないエラーが起きました")
+        }
     }
 
+    private func calcAction() throws {
+        guard let devidedInput = dividendTextField.text.flatMap({Double($0)}) else {
+            throw CalcError.dividendTextIsEmpty
+        }
+        guard let divisorInput = divisorTextField.text.flatMap({Double($0)}) else {
+            throw CalcError.divisorTextIsEmpty
+        }
+        guard divisorInput != 0 else {
+            throw CalcError.divisorIsZero
+        }
+        resultLabel.text = String(devidedInput/divisorInput)
+    }
 
+    private func showErrorAlert(_ message: String) {
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"OK", style: .default))
+        self.present(alert, animated: true)
+    }
 }
 
+fileprivate enum CalcError: Error {
+    case dividendTextIsEmpty
+    case divisorTextIsEmpty
+    case divisorIsZero
+}
